69c0684632130c7cbc039a9493c1630ef441ebce
PremChaurasiya07 <premchaurasiya2006@gmail.com>
2025-06-22 14:37:59 +0530

    cors issue

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Or restrict to your frontend origin e.g. ["http://localhost:3000"]
+    allow_origins=["http://localhost:3000","https://vyapari.vercel.app"],  # Or restrict to your frontend origin e.g. ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],  # Allow POST, GET, OPTIONS, etc.
     allow_headers=["*"],
